5ac7b0a94979a1deeaab25fc16286daf1ad792f9
Krenedits Ferenc <fefe.kre@gmail.com>
2024-12-03 09:47:14 +0100

    made function reusable

--- 3/3.ts
+++ 3/3.ts
@@ -1,35 +1,32 @@
 import { getData } from '../common';
 
 const data = getData('input.txt');
-const result1 = [...data.matchAll(/mul\((\d+),(\d+)\)/g)].reduce(
-    (acc, [, a, b]) => acc + +a * +b,
-    0,
-);
-const result2 = [...data.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g)].reduce(
-    (acc, match) => {
-        if (match[0] === 'do()') {
+const getResult = (firstTask: boolean) =>
+    [...data.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g)].reduce(
+        (acc, match) => {
+            if (match[0] === 'do()') {
+                return {
+                    ...acc,
+                    enabled: true,
+                };
+            }
+
+            if (match[0] === "don't()") {
+                return {
+                    ...acc,
+                    enabled: false,
+                };
+            }
+
             return {
                 ...acc,
-                enabled: true,
+                sum: acc.enabled || firstTask ? acc.sum + +match[1] * +match[2] : acc.sum,
             };
-        }
+        },
+        {
+            sum: 0,
+            enabled: true,
+        },
+    ).sum;
 
-        if (match[0] === "don't()") {
-            return {
-                ...acc,
-                enabled: false,
-            };
-        }
-
-        return {
-            ...acc,
-            sum: acc.enabled ? acc.sum + +match[1] * +match[2] : acc.sum,
-        };
-    },
-    {
-        sum: 0,
-        enabled: true,
-    },
-).sum;
-
-console.log(result1, result2);
+console.log(getResult(true), getResult(false));
