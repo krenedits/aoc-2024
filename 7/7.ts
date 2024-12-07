c2017e05bfcd1ef33736cfca021070806199b93a
Krenedits Ferenc <fefe.kre@gmail.com>
2024-12-07 13:07:54 +0100

    more optimization (~400ms)

--- 7/7.ts
+++ 7/7.ts
@@ -3,27 +3,32 @@ import { getLines } from '../common';
 const data = getLines('input.txt');
 console.time('time');
 
-const createNumber = (value: number, rest: number[], cache: number[], concat: boolean): void => {
+const createNumber = (value: number, rest: number[], target: number, concat: boolean): boolean => {
+    if (target < value) {
+        return false;
+    }
+
     if (rest.length > 0) {
         const [first, ...remaining] = rest;
-        createNumber(value + first, remaining, cache, concat);
-        createNumber(value * first, remaining, cache, concat);
+        const addition = createNumber(value + first, remaining, target, concat);
+        const multiplication = createNumber(value * first, remaining, target, concat);
+        let concatenation = false;
         if (concat) {
-            createNumber(+(value + '' + first), remaining, cache, concat);
+            concatenation = createNumber(+(value + '' + first), remaining, target, concat);
         }
-    } else {
-        cache.push(value);
+
+        return addition || multiplication || concatenation;
     }
+    
+    return value === target;
 };
 
 const {result1, result2} = data.reduce((acc, line) => {
     const [left, right] = line.split(': ');
     const [first, ...rest] = right.split(' ').map(Number);
-    const cacheOne: number[] = []
-    createNumber(first, rest, cacheOne, false);
-    const containsOne = cacheOne.includes(+left);
+    const target = +left;
 
-    if (containsOne) {
+    if (createNumber(first, rest, target, false)) {
         return {
             ...acc,
             result1: acc.result1 + +left,
@@ -31,11 +36,7 @@ const {result1, result2} = data.reduce((acc, line) => {
         };
     }
 
-    const cacheTwo: number[] = []
-    createNumber(first, rest, cacheTwo, true);
-    const containsTwo = cacheTwo.includes(+left);
-
-    if (containsTwo) {
+    if (createNumber(first, rest, target, true)) {
         return {
             ...acc,
             result2: acc.result2 + +left,
