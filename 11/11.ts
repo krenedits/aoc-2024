15d259531610996cc3e6bae98944789420a0fc9f
Krenedits Ferenc <fefe.kre@gmail.com>
2024-12-11 16:04:08 +0100

    day 11 improved (calculate 1000 rounds in ~5s)

--- 11/11.ts
+++ 11/11.ts
@@ -1,22 +1,21 @@
-import { getData } from "../common";
+import { getData } from '../common';
 
 const data = getData('input.txt');
 const numbers = data.split(' ').map(Number);
 const cache: Record<string, number> = {};
 
-const transform = (number: number): number[] => {    
+const transform = (number: number): number[] => {
     if (number === 0) {
         return [1];
-    } 
-    
-    if (Math.floor(Math.log10(number)) % 2 === 1) {
-        const first = Math.floor(number / 10 ** Math.ceil(Math.floor(Math.log10(number)) / 2));
-        const second = number - first * 10 ** Math.ceil(Math.floor(Math.log10(number)) / 2);
-        return [first, second];
+    }
+
+    const string = '' + number;
+    if (string.length % 2 === 0) {
+        return [+string.slice(0, string.length / 2), +string.slice(string.length / 2)];
     }
 
     return [number * 2024];
-} 
+};
 
 const simulation = (number: number, round: number) => {
     const key = round + '-' + number;
@@ -25,11 +24,10 @@ const simulation = (number: number, round: number) => {
         return result;
     }
 
-    
     if (round === 0) {
         return 1;
     }
-    
+
     const transformed = transform(number);
 
     const result = transformed.reduce((acc, number) => {
@@ -38,10 +36,10 @@ const simulation = (number: number, round: number) => {
     cache[key] = result;
 
     return result;
-}
+};
 
 console.time('start');
 const result1 = numbers.reduce((acc, number) => acc + simulation(number, 25), 0);
-const result2 = numbers.reduce((acc, number) => acc + simulation(number, 75), 0)
+const result2 = numbers.reduce((acc, number) => acc + simulation(number, 75), 0);
 console.timeEnd('start');
 console.log(result1, result2);
